f0770e2c1cf8310cf16dc84fab2630008ca8f04e
RicardoRaymundo <ricardoraymundao@gmail.com>
2023-01-09 20:51:33 +0000

    FEAT: implemented product list provider on main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/models/product_list.dart';
 import 'package:shop_app/pages/product_detail_page.dart';
 import 'package:shop_app/pages/products_overview_page.dart';
 import 'package:shop_app/utils/app_routes.dart';
@@ -12,19 +14,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          primarySwatch: Colors.purple,
-          fontFamily: 'Lato',
-          colorScheme: ThemeData()
-              .colorScheme
-              .copyWith(primary: Colors.purple, secondary: Colors.deepOrange)),
-      home: ProductsOverviewPage(),
-      routes: {
-        AppRoutes.productDetail:(context) => ProductDetailPage()
-      },
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            fontFamily: 'Lato',
+            colorScheme: ThemeData()
+                .colorScheme
+                .copyWith(primary: Colors.purple, secondary: Colors.deepOrange)),
+        home: ProductsOverviewPage(),
+        routes: {
+          AppRoutes.productDetail:(context) => ProductDetailPage()
+        },
+      ),
     );
   }
 }
