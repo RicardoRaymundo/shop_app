0c7f4a3c51c721ab1bc2fa69e026c5750781cdb7
RicardoRaymundo <ricardoraymundao@gmail.com>
2023-01-09 16:19:38 +0000

    FEAT: created product item component

---
+++ lib/components/product_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:shop_app/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({super.key, required this.product});
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            color: Theme.of(context).colorScheme.secondary,
+            icon: const Icon(Icons.favorite),
+            onPressed: () {},
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            color: Theme.of(context).colorScheme.secondary,
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {},
+          ),
+        ),
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
