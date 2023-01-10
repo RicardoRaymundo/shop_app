a2bf766d44eac1e6c8b685facba0b3b2009984d0
RicardoRaymundo <ricardoraymundao@gmail.com>
2023-01-10 12:24:21 +0000

    FEAT: added popup menu to toggle favorite list

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop_app/components/product_grid.dart';
+import 'package:shop_app/models/product_list.dart';
+
+enum FilterOptions { Favorite, All }
 
 class ProductsOverviewPage extends StatelessWidget {
-  ProductsOverviewPage({super.key});
+  const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha loja'),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (context) => [
+              const PopupMenuItem(
+                  value: FilterOptions.Favorite,
+                  child: Text('Somente favoritos')),
+              const PopupMenuItem(
+                  value: FilterOptions.All, child: Text('Todos')),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              print(selectedValue);
+              if (selectedValue == FilterOptions.Favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          ),
+        ],
       ),
-      body: ProductGrid(),
+      body: const ProductGrid(),
     );
   }
 }
