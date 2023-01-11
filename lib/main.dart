6942c546f2fbc7e7994194cba246415b9bdec1ac
RicardoRaymundo <ricardoraymundao@gmail.com>
2023-01-11 11:29:28 +0000

    FEAT: implemented multi provider, cart route and new theme color

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/models/cart.dart';
 import 'package:shop_app/models/product_list.dart';
+import 'package:shop_app/pages/cart_page.dart';
 import 'package:shop_app/pages/product_detail_page.dart';
 import 'package:shop_app/pages/products_overview_page.dart';
 import 'package:shop_app/utils/app_routes.dart';
@@ -14,20 +16,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ProductList(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ProductList()),
+        ChangeNotifierProvider(create: (_) => Cart())
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
             primarySwatch: Colors.purple,
             fontFamily: 'Lato',
-            colorScheme: ThemeData()
-                .colorScheme
-                .copyWith(primary: Colors.purple, secondary: Colors.deepOrange)),
+            colorScheme: ThemeData().colorScheme.copyWith(
+                primary: Colors.purple, secondary: Colors.deepOrange)),
         home: const ProductsOverviewPage(),
         routes: {
-          AppRoutes.productDetail:(context) => ProductDetailPage()
+          AppRoutes.productDetail: (context) => ProductDetailPage(),
+          AppRoutes.cart: (context) => CartPage(),
         },
       ),
     );
