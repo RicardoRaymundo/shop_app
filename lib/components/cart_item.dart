753d1526886691679a4ddf2b50b23a49616e1ed6
RicardoRaymundo <ricardoraymundao@gmail.com>
2023-01-17 14:06:54 +0000

    FEAT: updated cart item with dismissible and implemented provider on dismiss event

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/models/cart.dart';
 import 'package:shop_app/models/cart_item.dart';
 
 class CartItemWidget extends StatelessWidget {
@@ -8,24 +10,41 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      elevation: 2,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: EdgeInsets.all(5),
-              child: FittedBox(
-                child: Text('${cartItem.price}'),
+    return Dismissible(
+      key: ValueKey(cartItem.id),
+      direction: DismissDirection.endToStart,
+      onDismissed: (_) => Provider.of<Cart>(context, listen: false)
+          .removeItem(cartItem.productId),
+      background: Container(
+        alignment: Alignment.centerRight,
+        color: Theme.of(context).colorScheme.error,
+        padding: const EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+      ),
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        elevation: 2,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: EdgeInsets.all(5),
+                child: FittedBox(
+                  child: Text('${cartItem.price}'),
+                ),
               ),
             ),
+            title: Text(cartItem.title),
+            subtitle: Text(
+                'Total R\$ ${(cartItem.price * cartItem.quantity).toStringAsFixed(2)}'),
+            trailing: Text('${cartItem.quantity}x'),
           ),
-          title: Text(cartItem.title),
-          subtitle: Text(
-              'Total R\$ ${(cartItem.price * cartItem.quantity).toStringAsFixed(2)}'),
-          trailing: Text('${cartItem.quantity}x'),
         ),
       ),
     );
