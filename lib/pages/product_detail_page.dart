8e71541ab58f22dd5aff5b6b36e302f38e7622a1
RicardoRaymundo <ricardoraymundao@gmail.com>
2023-01-17 14:07:54 +0000

    FEAT: updated product detail page

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -4,13 +4,41 @@ import 'package:shop_app/models/product.dart';
 class ProductDetailPage extends StatelessWidget {
   const ProductDetailPage({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-  final Product product = ModalRoute.of(context)!.settings.arguments as Product; 
+    final Product product =
+        ModalRoute.of(context)!.settings.arguments as Product;
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'R\$ ${product.price.toStringAsFixed(2)}',
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(product.description, textAlign: TextAlign.center,),
+            )
+          ],
+        ),
       ),
     );
   }
