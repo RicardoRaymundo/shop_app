cc6e92a38d412b75400b671cbc4fcdd2edf629df
RicardoRaymundo <ricardoraymundao@gmail.com>
2023-01-25 14:49:10 +0000

    FEAT: created constants file for Urls

---
+++ lib/utils/constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static const productBaseUrl = 'https://shop-app-ed049-default-rtdb.europe-west1.firebasedatabase.app/products';
+  static const orderBaseUrl = 'https://shop-app-ed049-default-rtdb.europe-west1.firebasedatabase.app/orders';
+}
