d1ecb266aee3e66c368762032541121b203d00ed
RicardoRaymundo <ricardoraymundao@gmail.com>
2023-01-25 15:07:17 +0000

    FEAT: refactored component to best async performance

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -45,17 +45,7 @@ class CartPage extends StatelessWidget {
                     ),
                   ),
                   const Spacer(),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                        textStyle: TextStyle(
-                            color: Theme.of(context).colorScheme.primary)),
-                    onPressed: () {
-                      Provider.of<OrderList>(context, listen: false)
-                          .addOrder(cart);
-                      cart.clear();
-                    },
-                    child: const Text('COMPRAR'),
-                  )
+                  CartBuyButton(cart: cart)
                 ],
               ),
             ),
@@ -71,5 +61,47 @@ class CartPage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class CartBuyButton extends StatefulWidget {
+  const CartBuyButton({
+    Key? key,
+    required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  State<CartBuyButton> createState() => _CartButtonState();
+}
+
+class _CartButtonState extends State<CartBuyButton> {
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return _isLoading
+        ? CircularProgressIndicator()
+        : TextButton(
+            style: TextButton.styleFrom(
+                textStyle:
+                    TextStyle(color: Theme.of(context).colorScheme.primary)),
+            onPressed: widget.cart.itemsCount == 0
+                ? null
+                : () async {
+                    setState(() => _isLoading = true);
+
+                    await Provider.of<OrderList>(
+                      context,
+                      listen: false,
+                    ).addOrder(widget.cart);
+
+                    widget.cart.clear();
+
+                    setState(() => _isLoading = false);
+                  },
+            child: const Text('COMPRAR'),
+          );
   }
 }
